bd8fc5e6b472127530a4ce2dda1872fba7503048
MisticVoid <misticvoid@gmail.com>
2021-04-01 06:22:24 +0200

    access to rotate restricted

--- PlayerVisual.py
+++ PlayerVisual.py
@@ -3,8 +3,9 @@ from pygame import *
 from Player import *
 from math import pi,sqrt
 
+
 class PlayerVisual:
-    def __init__(self,sizeX: int,sizeY: int, bodyColor: Color,EdgeColor: Color,sourcePlayer: Player,edgeWidth: int =10):
+    def __init__(self,sizeX: int,sizeY: int, bodyColor: Color,EdgeColor: Color,sourcePlayer,edgeWidth: int =10):
         self.sizeX= sizeX
         self.sizeY = sizeY
         self.surface = Surface((sizeX,sizeY),SRCALPHA)
@@ -18,13 +19,13 @@ class PlayerVisual:
         self.surface.fill((0,0,0,0))
         draw.polygon(self.surface, self.EdgeColor, [(0, 0), (0, self.sizeY), (self.sizeX, self.sizeY // 2)])
         draw.polygon(self.surface,self.bodyColor,[(self.sqrtEdge,self.sqrtEdge),(self.sqrtEdge,self.sizeY-self.sqrtEdge),(self.sizeX-self.edgeWidth,self.sizeY//2)])
-        return self.rotate(self.sourcePlayer.angle)
+        return self.__rotate(self.sourcePlayer.angle)
 
     def recolorEdge(self,value: float) -> Color:
         self.EdgeColor = Color(int(min(255*(2-2*value),255)),int(min(255*value*2,255)),0)
         return self.EdgeColor
 
-    def rotate(self,angle: float) -> Surface:
+    def __rotate(self,angle: float) -> Surface:
         return transform.rotate(self.surface,angle*180/pi)
 
 
