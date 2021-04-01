f46e2a351942a57c4388e45601e48cd347e7eefa
MisticVoid <misticvoid@gmail.com>
2021-04-01 05:23:41 +0200

    payer color correct

--- PlayerVisual.py
+++ PlayerVisual.py
@@ -1,10 +1,10 @@
 from typing import *
 from pygame import *
 from Player import *
-from math import pi
+from math import pi,sqrt
 
 class PlayerVisual:
-    def __init__(self,sizeX: int,sizeY: int, bodyColor: Color,EdgeColor: Color,sourcePlayer: Player,edgeWidth: int =5):
+    def __init__(self,sizeX: int,sizeY: int, bodyColor: Color,EdgeColor: Color,sourcePlayer: Player,edgeWidth: int =10):
         self.sizeX= sizeX
         self.sizeY = sizeY
         self.surface = Surface((sizeX,sizeY),SRCALPHA)
@@ -12,11 +12,12 @@ class PlayerVisual:
         self.EdgeColor = EdgeColor
         self.sourcePlayer = sourcePlayer
         self.edgeWidth = edgeWidth
+        self.sqrtEdge = sqrt(edgeWidth)
 
     def draw(self) -> Surface:
         self.surface.fill((0,0,0,0))
-        draw.polygon(self.surface,self.bodyColor,[(0,0),(0,self.sizeY),(self.sizeX,self.sizeY//2)])
-        draw.polygon(self.surface, self.EdgeColor, [(0, 0), (0, self.sizeY), (self.sizeX, self.sizeY // 2)], self.edgeWidth)
+        draw.polygon(self.surface, self.EdgeColor, [(0, 0), (0, self.sizeY), (self.sizeX, self.sizeY // 2)])
+        draw.polygon(self.surface,self.bodyColor,[(self.sqrtEdge,self.sqrtEdge),(self.sqrtEdge,self.sizeY-self.sqrtEdge),(self.sizeX-self.edgeWidth,self.sizeY//2)])
         return self.rotate(self.sourcePlayer.angle)
 
     def recolorEdge(self,value: float) -> Color:
